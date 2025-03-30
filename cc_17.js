7b5563aefec3cdd709306c9671d72f772ca3ddc1
Fernando <mhfernando778@gmail.com>
2025-03-30 16:31:51 -0400

    Task 1 - creating a customer class

---
+++ cc_17.js
@@ -0,0 +1,15 @@
+class Customer {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.purchaseHistory = [];
+    }
+
+    addPurchase(amount) {
+        this.purchaseHistory.push(amount);
+    }
+
+    getTotalSpent() {
+        return this.purchaseHistory.reduce((total, amount) => total + amount, 0);
+    }
+}
